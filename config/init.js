df7dd5f6d62b95eb3390bfe182174cfe20baad0f
phuongNA <nap810@gmail.com>
2015-08-07 14:37:11 +0700

    remove admin checkbox anf demo connect multiple mongoDB

--- config/init.js
+++ config/init.js
@@ -21,7 +21,7 @@ module.exports = function() {
 			if (process.env.NODE_ENV) {
 				console.error(chalk.red('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead'));
 			} else {
-				console.error(chalk.red('NODE_ENV is not defined! Using default development environment'));
+				console.error(chalk.red(' Using default development environment'));
 			}
 
 			process.env.NODE_ENV = 'development';
